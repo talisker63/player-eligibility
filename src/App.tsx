4dd2a400a562007ed87002e488b70d237fcf025f
talisker63 <asleighty@gmail.com>
2026-02-19 17:00:27 +1100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Player Eligibility</h1>
+    </div>
+  )
+}
+
+export default App
